92444622a6c2fcf2950373f9f3a87922f9247cd2
Jewel Rana <tonujewel@gmail.com>
2020-11-04 22:38:14 +0600

    hh

--- lib/controller/shopping_controller.dart
+++ lib/controller/shopping_controller.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_getx_practice/model/product.dart';
 import 'package:get/get.dart';
 
 class ShoppingController extends GetxController{
 
+  var products = List<Product>().obs;
 }
--- lib/views/shopping_page.dart
+++ lib/views/shopping_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_practice/controller/shopping_controller.dart';
+import 'package:get/get.dart';
 
 class ShoppingPage extends StatelessWidget {
+  final shoppingController = Get.put(ShoppingController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,12 +12,27 @@ class ShoppingPage extends StatelessWidget {
         child: Column(
           children: [
             Expanded(
-              child: ListView.builder(itemBuilder: (context, index){
-                return Card();
+              child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                return Card(
+                  child: Column(
+                    children: [
+                      Text('First Product', style: TextStyle(fontSize: 22)),
+                      Text('First Product'),
+                      RaisedButton(
+                        onPressed: () {},
+                        child: new Text(
+                          "Add to cart",
+                        ),
+                      )
+                    ],
+                  ),
+                );
               }),
             ),
-
             Text('Total Amount'),
+            SizedBox(height: 100,)
           ],
         ),
       ),
